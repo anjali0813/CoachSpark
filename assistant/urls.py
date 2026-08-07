ec8fbabd7089c0d5bafbccced7489d2c2bfc893e
Anjali Binuprasad <anjalibinuprasad@gmail.com>
2026-08-07 16:54:26 +0530

    Merge remote changes

--- assistant/urls.py
+++ assistant/urls.py
@@ -1,22 +1,3 @@
-# """
-# urls.py - Coach Spark app routes
-
-# If your project's main urls.py doesn't already include this app,
-# add: path('', include('assistant.urls'))
-# """
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.index, name='index'),
-#     path('chat/', views.chat_api, name='chat_api'),
-#     path('quiz/start/', views.quiz_start, name='quiz_start'),
-#     path('quiz/answer/', views.quiz_answer, name='quiz_answer'),
-# ]
-
-
-
-
 """
 urls.py - Coach Spark app routes
 
@@ -31,5 +12,4 @@ urlpatterns = [
     path('chat/', views.chat_api, name='chat_api'),
     path('quiz/start/', views.quiz_start, name='quiz_start'),
     path('quiz/answer/', views.quiz_answer, name='quiz_answer'),
-    path('quiz/history/', views.quiz_history_view, name='quiz_history'),
 ]
